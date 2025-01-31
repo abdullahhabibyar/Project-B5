db996776c8d28b3e6fa1a1799ce3481227e2a058
ahabi <ahabi@Abdullah.fios-router.home>
2025-01-30 20:26:19 -0500

    Implmented the code you can check and if there wa problom updet me

---
+++ src/Demo1/DemoTestB1.java
@@ -0,0 +1,29 @@
+package Demo1;
+
+public class DemoTestB1 {
+	public class VariableExample {
+	    // Instance variable
+	    int instanceVar = 10;
+
+	    // Static variable (shared by all objects)
+	    static int staticVar = 20;
+
+	    public void showVariables() {
+	        // Local variable
+	        int localVar = 5;
+
+	        System.out.println("Local Variable: " + localVar);
+	        System.out.println("Instance Variable: " + instanceVar);
+	        System.out.println("Static Variable: " + staticVar);
+	    }
+
+	    public static void main(String[] args) {
+	        // Creating an object to access instance variables
+	        VariableExample obj = new VariableExample();
+	        obj.showVariables();
+	    }
+	}
+	
+	
+
+}
